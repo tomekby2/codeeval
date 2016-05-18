71c904442a392a6e6b0bb5b0cd5b01efd241893b
tomekby2 <github2@tomekby.vot.pl>
2016-05-19 00:48:18 +0200

    Update 16.php

--- 16.php
+++ 16.php
@@ -5,21 +5,21 @@
  */
 
 function countOnes($number) {
-	$number = (int)$number;
+    $number = (int)$number;
 
-	$count = 0;
-	while($number != 0) {
-		$count += $number & 1;
-		$number >>= 1;
-	}
-	return $count;
+    $count = 0;
+    while($number != 0) {
+        $count += $number & 1;
+        $number >>= 1;
+    }
+    return $count;
 }
 
 $fh = fopen($argv[1], "r");
 while (true) {
-	$test = fgets($fh);
-	if($test == '') break;
+    $test = fgets($fh);
+    if($test == '') break;
 
-	$str = sprintf("%d\n", countOnes($test));
-	fwrite(STDOUT, $str);
+    $str = sprintf("%d\n", countOnes($test));
+    fwrite(STDOUT, $str);
 }
